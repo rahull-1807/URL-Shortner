2fe41778ba267ce9f0bc1530330290448ab29c04
rahull-1807 <rahulchouhan8002@gmail.com>
2026-01-16 16:48:39 +0530

    fix: use environment variables for MongoDB

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const port = 5000;  
+const port = process.env.PORT || 5000;
+ 
 const urlRoutes = require("./routes/url");
 const { connectDB } = require("./routes/connect");
 
 const URL = require("./models/url");
 
-connectDB("mongodb://localhost:27017/urlShortenerDB").then(() => {
+connectDB(process.env.MONGO_URI).then(() => {
   console.log("Connected to MongoDB");
-}
-)
+});
+
 
 app.get("/health", (req, res) => {
   res.json({ status: "Backend is running" });
